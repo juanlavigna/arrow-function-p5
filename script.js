af319200680236b3d564f3326a7d58113cad3dc2
Juan Lavigna <juanlavigna@gmail.com>
2022-06-05 13:49:39 -0300

    Inserta el codigo del ejercicio 1

---
+++ script.js
@@ -0,0 +1,12 @@
+function sumarLosTres(num1, num2, num3) {
+    return num1 + num2 + num3
+   }
+   
+   function cuadrado(num) {
+    return num * num
+   }
+   
+   function decirHola() {
+    console.log('¡Hola!');
+   }
+
